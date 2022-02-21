71df1a562af1830af7b220cf488bf8d1a7b14103
DanHavely <dhavely@googlemail.com>
2022-02-21 06:13:36 +0000

    R analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -15,7 +15,7 @@ features$feature_label <- gsub("Gyro","gyroscope_",  features$feature_label)
 features$feature_label <- gsub("Mag","magnitude_",  features$feature_label)
 features$feature_label <- gsub("Jerk","jerk_",  features$feature_label)
 features$feature_label <- gsub("-(.*)-(.+)$", "\\2axis_\\1", features$feature_label)
-features$feature_label <- gsub(" |-|,|\\(\\)", "", x_cols$feature_label)
+features$feature_label <- gsub(" |-|,|\\(\\)", "", features$feature_label)
 
 # Load training data
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
@@ -44,7 +44,7 @@ activity_tracking$activity_id <- as.factor(activity_tracking$activity_id)
 levels(activity_tracking$activity_id) <- activity_labels$activity_label
 
 # Select out only those fields with means and standard deviations by name - I'm ignoring the additional freq mean columns
-activ_track_mean_std <- activity_tracking[,c(c(TRUE,TRUE,TRUE),grepl("mean$|std$",x_cols$feature_label))]
+activ_track_mean_std <- activity_tracking[,c(c(TRUE,TRUE,TRUE),grepl("mean$|std$",features$feature_label))]
 
 # Summarise with mean of all measures by activity and subject
 activ_track_summary <- activ_track_mean_std %>% select(-study_phase) %>% group_by(activity_id, subject_id) %>% summarise_all(mean) 
